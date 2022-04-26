70df9ee8523c306035a0d8ee69bb28bf2fd8a21e
Rob Whitaker <rw@rwapp.co.uk>
2022-04-26 20:34:34 +0100

    Initial Commit

---
+++ Telescope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telescope
+//
+//  Created by Rob Whitaker on 26/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
